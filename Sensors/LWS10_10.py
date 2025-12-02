00a8102dbba61eb7c56ce4c48e8543f3946141ee
AlaminBantan <143328322+AlaminBantan@users.noreply.github.com>
2025-12-02 15:06:33 +0300

    Update LWS10_10.py

--- Sensors/LWS10_10.py
+++ Sensors/LWS10_10.py
@@ -1,22 +1,22 @@
 import minimalmodbus
 from time import sleep
 
-LW_1 = minimalmodbus.Instrument('/dev/ttyUSB0', 10)
+LW_10 = minimalmodbus.Instrument('/dev/ttyUSB0', 10)
 
-LW_1.serial.baudrate = 19200
-LW_1.serial.bytesize = 8
-LW_1.serial.parity   = minimalmodbus.serial.PARITY_EVEN
-LW_1.serial.stopbits = 1
-LW_1.serial.timeout  = 0.5
-LW_1.mode = minimalmodbus.MODE_RTU
+LW_10.serial.baudrate = 19200
+LW_10.serial.bytesize = 8
+LW_10.serial.parity   = minimalmodbus.serial.PARITY_EVEN
+LW_10.serial.stopbits = 1
+LW_10.serial.timeout  = 0.5
+LW_10.mode = minimalmodbus.MODE_RTU
 
-LW_1.clear_buffers_before_each_transaction = True
-LW_1.close_port_after_each_call = True
+LW_10.clear_buffers_before_each_transaction = True
+LW_10.close_port_after_each_call = True
 
 try:
     while True:
         # Temperature (°C)
-        temp_c = LW_1.read_register(
+        temp_c = LW_10.read_register(
             0,
             number_of_decimals=2,
             functioncode=3,
@@ -24,7 +24,7 @@ try:
         )
 
         # Wetness (%)
-        wetness = LW_1.read_register(
+        wetness = LW_10.read_register(
             1,
             number_of_decimals=2,
             functioncode=3,
@@ -39,5 +39,5 @@ try:
         sleep(1)
 
 except KeyboardInterrupt:
-    LW_1.serial.close()
+    LW_10.serial.close()
     print("Ports Now Closed")
