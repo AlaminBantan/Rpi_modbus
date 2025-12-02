7abbedec26447e4ff747785d4c9de94f2a859c4e
AlaminBantan <143328322+AlaminBantan@users.noreply.github.com>
2025-12-02 15:52:27 +0300

    Update SM2121_changesettings.py

--- Sensors/SM2121_changesettings.py
+++ Sensors/SM2121_changesettings.py
@@ -12,9 +12,9 @@ ph.clear_buffers_before_each_transaction = True
 ph.close_port_after_each_call = True
 
 # 1️⃣ Change baud rate to 19200 (code 4) at register 103
-ph.write_register(103, 4, functioncode=6)
+ph.write_register(67, 4, functioncode=6)
 
 # 2️⃣ Change device ID to 21 at register 102
-ph.write_register(102, 21, functioncode=6)
+ph.write_register(66, 21, functioncode=6)
 
 print("Changed baud to 19200 and address to 21. Update your serial settings accordingly.")
